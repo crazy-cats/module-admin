a7648c0e037a6ac34dcc49ea13467dc7448e6acc
Bruce <bruce.z@infinitytesting.com.au>
2018-11-30 21:56:39 +0800

    ^ build Administrator module

--- config/backend/menu.php
+++ config/backend/menu.php
@@ -13,7 +13,7 @@
  */
 return [
         [
-        'label' => __( 'Administrator' ), 'identifier' => 'admin', 'url' => getUrl( 'admin/admin' ),
+        'label' => __( 'Admin' ), 'identifier' => 'admin',
         'children' => [
                 [ 'label' => __( 'Administrators' ), 'identifier' => 'admin-list', 'url' => getUrl( 'admin/admin' ) ],
                 [ 'label' => __( 'Administrator Roles' ), 'identifier' => 'admin-roles', 'url' => getUrl( 'admin/adminrole' ) ]
