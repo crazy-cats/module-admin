6c8e698a617cf6fb24bf44eeda9c45be761e916a
Bruce <bruce.z@infinitytesting.com.au>
2018-12-10 17:37:08 +0800

    ^ build Administrator module

--- code/Block/Admin/Role/Edit.php
+++ code/Block/Admin/Role/Edit.php
@@ -7,6 +7,10 @@
 
 namespace CrazyCat\Admin\Block\Admin\Role;
 
+use CrazyCat\Admin\Model\Admin\Role;
+use CrazyCat\Framework\App\Theme\Block\Context;
+use CrazyCat\Framework\Utility\Tools;
+
 /**
  * @category CrazyCat
  * @package CrazyCat\Admin
@@ -16,6 +20,18 @@ namespace CrazyCat\Admin\Block\Admin\Role;
 class Edit extends \CrazyCat\Framework\App\Module\Block\Backend\AbstractEdit {
 
     /**
+     * @var \CrazyCat\Admin\Model\Admin\Role
+     */
+    protected $role;
+
+    public function __construct( Role $role, Context $context, array $data = [] )
+    {
+        parent::__construct( $context, $data );
+
+        $this->role = $role;
+    }
+
+    /**
      * @return array
      */
     public function getFields()
@@ -23,7 +39,7 @@ class Edit extends \CrazyCat\Framework\App\Module\Block\Backend\AbstractEdit {
         return [
                 [ 'name' => 'id', 'label' => __( 'ID' ), 'type' => 'hidden' ],
                 [ 'name' => 'title', 'label' => __( 'Title' ), 'type' => 'text' ],
-                [ 'name' => 'permisions', 'label' => __( 'Permisions' ), 'type' => 'multiselect' ]
+                [ 'name' => 'permisions', 'label' => __( 'Permisions' ), 'type' => 'multiselect', 'options' => Tools::toOptionsArray( $this->role->getAllPermissions() ) ]
         ];
     }
 
--- code/Model/Admin/Role.php
+++ code/Model/Admin/Role.php
@@ -7,6 +7,11 @@
 
 namespace CrazyCat\Admin\Model\Admin;
 
+use CrazyCat\Framework\App\Area;
+use CrazyCat\Framework\App\Db\Manager as DbManager;
+use CrazyCat\Framework\App\EventManager;
+use CrazyCat\Framework\App\Module\Manager as ModuleManager;
+
 /**
  * @category CrazyCat
  * @package CrazyCat\Admin
@@ -16,11 +21,45 @@ namespace CrazyCat\Admin\Model\Admin;
 class Role extends \CrazyCat\Framework\App\Module\Model\AbstractModel {
 
     /**
+     * @var array
+     */
+    static protected $permissions;
+
+    /**
+     * @var \CrazyCat\Framework\App\Module\Manager
+     */
+    protected $moduleManager;
+
+    public function __construct( ModuleManager $moduleManager, EventManager $eventManager, DbManager $dbManager, array $data = [] )
+    {
+        parent::__construct( $eventManager, $dbManager, $data );
+
+        $this->moduleManager = $moduleManager;
+    }
+
+    /**
      * @return void
      */
     protected function construct()
     {
         $this->init( 'admin_role', 'admin_role' );
+    }
+
+    /**
+     * @return array
+     */
+    public function getAllPermissions()
+    {
+        if ( self::$permissions === null ) {
+            $actions = [];
+            foreach ( $this->moduleManager->getEnabledModules() as $module ) {
+                $actions = array_merge( $actions, array_keys( $module->getControllerActions( Area::CODE_BACKEND ) ) );
+            }
+            sort( $actions );
+            self::$permissions = array_combine( $actions, $actions );
+        }
+
+        return self::$permissions;
     }
 
 }
