44ea2f747a35afc1c39f7d7c636a3e5e4bc4391e
Bruce <bruce.z@infinitytesting.com.au>
2018-12-14 11:29:30 +0800

    ^ build Administrator module

--- code/Block/Admin/Grid.php
+++ code/Block/Admin/Grid.php
@@ -7,6 +7,10 @@
 
 namespace CrazyCat\Admin\Block\Admin;
 
+use CrazyCat\Core\Model\Source\YesNo as SourceYesNo;
+use CrazyCat\Framework\App\Session\Backend as Session;
+use CrazyCat\Framework\App\Theme\Block\Context;
+
 /**
  * @category CrazyCat
  * @package CrazyCat\Admin
@@ -18,6 +22,18 @@ class Grid extends \CrazyCat\Framework\App\Module\Block\Backend\AbstractGrid {
     const BOOKMARK_KEY = 'admin';
 
     /**
+     * @var \CrazyCat\Core\Model\Source\YesNo
+     */
+    protected $sourceYesNo;
+
+    public function __construct( SourceYesNo $sourceYesNo, Session $session, Context $context, array $data = array() )
+    {
+        parent::__construct( $session, $context, $data );
+
+        $this->sourceYesNo = $sourceYesNo;
+    }
+
+    /**
      * @return array
      */
     public function getFields()
@@ -27,6 +43,7 @@ class Grid extends \CrazyCat\Framework\App\Module\Block\Backend\AbstractGrid {
                 [ 'name' => 'id', 'label' => __( 'ID' ), 'sort' => true, 'width' => 100, 'filter' => [ 'type' => 'text', 'condition' => 'eq' ] ],
                 [ 'name' => 'name', 'label' => __( 'Name' ), 'sort' => true, 'filter' => [ 'type' => 'text', 'condition' => 'like' ] ],
                 [ 'name' => 'username', 'label' => __( 'Username' ), 'sort' => true, 'filter' => [ 'type' => 'text', 'condition' => 'like' ] ],
+                [ 'name' => 'enabled', 'label' => __( 'Enabled' ), 'sort' => true, 'width' => 130, 'filter' => [ 'type' => 'select', 'condition' => 'eq', 'options' => $this->sourceYesNo->toOptionsArray() ] ],
                 [ 'label' => __( 'Actions' ), 'actions' => [
                         [ 'name' => 'edit', 'label' => __( 'Edit' ), 'url' => getUrl( 'admin/admin/edit' ) ],
                         [ 'name' => 'delete', 'label' => __( 'Delete' ), 'confirm' => __( 'Sure you want to remove this item?' ), 'url' => getUrl( 'admin/admin/delete' ) ]
